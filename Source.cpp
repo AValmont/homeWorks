7a3d007195bd037ea766f04e3df0c3f65fc4c79a
AValmont <as.valmont@gmail.com>
2025-12-14 23:47:36 +0300

    Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
+#include <Windows.h>
 
 
 int main()
 {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     int num;
     
-    std::cout << "Enter your number:" << std::endl;
+    std::cout << "Введите число:" << std::endl;
     std::cin >> num;
-    std::cout << "You entered the number:" <<std::endl;
+    std::cout << "Вы ввели:" <<std::endl;
     std::cout << num << std::endl;
-    std::cout << "\n\n";
 
-    std::cout << "Enter your word" << std::endl;
+    std::cout << "Введите слово:" << std::endl;
     std::string new_word;
     std::cin >> new_word;
-    std::cout << "You enteder the word:" << std::endl;
+    std::cout << "Вы ввели: " << std::endl;
     std::cout << new_word << std::endl;
-    
-
     
     return EXIT_SUCCESS;
 }
+
